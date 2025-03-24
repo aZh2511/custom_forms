ba599da66197f8e2d0bcbcab155c372ea304d8dc
Andrii Zheravin <andrii.zheravin@affirm.com>
2025-03-24 19:54:32 +0100

    fix

--- tests/mocks/core/domain/repositories.py
+++ tests/mocks/core/domain/repositories.py
@@ -16,17 +16,22 @@ class TestsRepository(IRepository):
         self._tables[table_name].add(record)
 
     def _get_by_id(self, table_name: str, uuid: UUID) -> Aggregate | None:
-        maybe_record = next(filter(
-            lambda r: r.uuid.value == uuid,
-            self._tables[table_name],
-        ), None)
+        maybe_record = next(
+            filter(
+                lambda r: r.uuid.value == uuid,
+                self._tables[table_name],
+            ),
+            None,
+        )
         return maybe_record
 
     def get_form_by_uuid(self, form_uuid: FormUUID) -> Form | None:
-        return self._get_by_id('form', form_uuid.value)
+        return self._get_by_id("form", form_uuid.value)
 
-    def get_form_response_by_uuid(self, form_uuid: FormResponseUUID) -> FormResponse | None:
-        return self._get_by_id('form_response', form_uuid.value)
+    def get_form_response_by_uuid(
+        self, form_uuid: FormResponseUUID
+    ) -> FormResponse | None:
+        return self._get_by_id("form_response", form_uuid.value)
 
     def save_form_response(self, form_response: FormResponse) -> None:
-        self.add('form_response', form_response)
+        self.add("form_response", form_response)
