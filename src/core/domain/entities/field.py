bb131ceb213e8f831b3925156fe0542296b7b314
Andrii Zheravin <andrii.zheravin@affirm.com>
2025-03-24 10:42:51 +0100

    Field is_required

--- src/core/domain/entities/field.py
+++ src/core/domain/entities/field.py
@@ -8,13 +8,24 @@ from typing import Any
 class Field(Entity):
     uuid: FieldUUID
     value: Any = None
-    allow_null: bool = False
+
+    _is_required: bool = False
+
+    @property
+    def is_required(self) -> bool:
+        return self._is_required
 
     @classmethod
     def create(cls) -> "Field":
         return Field(
             uuid=FieldUUID(),
         )
+
+    def mark_required(self) -> None:
+        self._is_required = True
+
+    def mark_not_required(self) -> None:
+        self._is_required = False
 
     def __hash__(self) -> int:
         return hash(self.uuid)
--- tests/core/domain/entities/test_field.py
+++ tests/core/domain/entities/test_field.py
@@ -6,3 +6,26 @@ def test_field_create_results_in_unique_forms() -> None:
     field2 = Field.create()
 
     assert field1 != field2
+
+
+def test_non_required_by_default() -> None:
+    field = Field.create()
+
+    assert not field.is_required
+
+
+def test_mark_required() -> None:
+    field = Field.create()
+
+    field.mark_required()
+
+    assert field.is_required is True
+
+
+def test_mark_not_required() -> None:
+    field = Field.create()
+    field.mark_required()
+
+    field.mark_not_required()
+
+    assert not field.is_required
